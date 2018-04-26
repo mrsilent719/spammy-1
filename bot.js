b5fb7093148328602a2a1b8900b783b4d8ed80c5
Matias Perez <cepiperez@gmail.com>
2018-04-26 10:40:11 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', message => {
     
     if (message.content === '$ping') {
     	message.reply('pong');
-  	}
+    }
     
     if (message.content === "$spam") { 
         if (spamid.indexOf(message.channel) < 0)
