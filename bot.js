40e2571311a509d778383828f79b8a30c39e7edc
Matias Perez <cepiperez@gmail.com>
2018-04-26 12:34:06 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const client = new Discord.Client();
 const images = JSON.parse(fs.readFileSync("./pokemonrefs.json", "utf8"));
 
 //var interval = 5000;
-//var spamid = [];
-//var infoid = [];
+var spamid = [];
+var infoid = [];
 
 /*function step() {
     var index;
@@ -26,8 +26,8 @@ client.on('message', message => {
     }
     
     /*if (message.content === '$spam') { 
-        if (spamid.indexOf(message.channel) < 0) {
-            spamid.push(message.channel);
+        if (spamid.indexOf(message.channel.id) < 0) {
+            spamid.push(message.channel.id);
         }
         message.channel.send('spam enabled');
         
@@ -40,7 +40,7 @@ client.on('message', message => {
     }*/
     
     /*if (message.content === '$stop') {
-        var index = spamid.indexOf(message.channel);
+        var index = spamid.indexOf(message.channel.id);
         if (index > -1) {
           spamid.splice(index, 1);
         }
@@ -63,12 +63,12 @@ client.on('message', message => {
     }*/
 
     /*if (message.content === '$info') {
-        var index = infoid.indexOf(message.channel);
+        var index = infoid.indexOf(message.channel.id);
         if (index > -1) {
             infoid.splice(index, 1);
             message.channel.send('spawns info disabled');
         } else {
-            infoid.push(message.channel);
+            infoid.push(message.channel.id);
             message.channel.send('spawns info enabled');
         }
     }*/
@@ -79,7 +79,7 @@ client.on('message', message => {
 
     if (message.embeds.length > 0) {
         emb = message.embeds[0];
-        if (emb.title.startsWith('A wild') && infoid.indexOf(message.channel)>-1) {
+        if (emb.title.startsWith('A wild') && infoid.indexOf(message.channel.id)>-1) {
             //message.channel.send(emb.image.url);
             name = emb.image.url.split('/').pop(-1).split('.')[0];
             //message.channel.send(images[name]);
