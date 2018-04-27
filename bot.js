ecc95224fd9e30fdc96c5533aa5d54585a2571dd
Matias Perez <cepiperez@gmail.com>
2018-04-27 09:25:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,15 +77,17 @@ client.on('message', message => {
         message.channel.send('spawns info channels: ' + infoid.join(' '));
     }
 
-    if (message.embeds.length > 0) {
-        emb = message.embeds[0];
-        if (emb.title.startsWith('A wild') && infoid.indexOf(message.channel.id)>-1) {
-            //message.channel.send(emb.image.url);
-            name = emb.image.url.split('/').pop(-1).split('.')[0];
-            //message.channel.send(images[name]);
-            realname = images[name];
-            if (realname.length >0)
-                message.channel.send('A wild ' + realname + ' has appeared');
+    if (infoid.indexOf(message.channel.id) > -1) {
+        if (message.embeds.length > 0) {
+            emb = message.embeds[0];
+            if (emb.title.startsWith('A wild')) {
+                //message.channel.send(emb.image.url);
+                name = emb.image.url.split('/').pop(-1).split('.')[0];
+                //message.channel.send(images[name]);
+                realname = images[name];
+                if (realname.length >0)
+                    message.channel.send('A wild ' + realname + ' has appeared');
+            }
         }
     }
     
